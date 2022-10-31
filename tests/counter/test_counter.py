4c6ebf790df0eeffb4e452781bf7e1e947a180de
IgorHamzi <igorwhamzi@gmail.com>
2022-10-31 08:45:35 -0300

    test: refactor test_counter

--- tests/counter/test_counter.py
+++ tests/counter/test_counter.py
@@ -3,6 +3,6 @@ from src.counter import count_ocurrences
 
 def test_counter():
     result_python = count_ocurrences("src/jobs.csv", "python") == 1639
-    result_javascript = count_ocurrences("src/jobs.csv", "javascript") == 1639
+    result_javascript = count_ocurrences("src/jobs.csv", "javascript") == 122
     assert result_python
     assert result_javascript
